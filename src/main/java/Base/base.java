bf785c1329bf95d4f32764b7e204162a6794b728
a-uzzaman <a.uzzaman1206@gmail.com>
2019-09-22 15:33:06 -0400

    Merge branch 'groupProject_Saju' of https://github.com/a-uzzaman/OrangeHRMGroupProject into groupProject_Saju

--- src/main/java/Base/base.java
+++ src/main/java/Base/base.java
@@ -1,5 +1,42 @@
 package Base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+
 public class base {
 
+	public WebDriver driver;
+
+	
+	@BeforeTest
+	public void beforeSuite(String browser){
+		
+		
+		System.out.println("*** TEST SUITE STARTED ");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\Drivers\\chromedriver.exe");
+		
+
+
+		driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		driver.manage().window().maximize();
+		}
+		
+	@AfterTest
+	public void afterSuite(){
+		System.out.println("*** TEST SUITE ENDED ");
+		driver.quit();
+		
+
+	}
+
+
+
+
+}
+	
 }
