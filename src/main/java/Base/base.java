1d2824b07812b63b1e9ad1bf15712cf29f5090b0
a-uzzaman <a.uzzaman1206@gmail.com>
2019-09-22 16:10:39 -0400

    updated

--- src/main/java/Base/base.java
+++ src/main/java/Base/base.java
@@ -3,15 +3,16 @@ package Base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
 public class base {
 
 	public WebDriver driver;
 
 	
-	@BeforeTest
-	public void beforeSuite(String browser){
+	@BeforeSuite
+	public void beforeSuite(){
 		
 		
 		System.out.println("*** TEST SUITE STARTED ");
@@ -30,6 +31,11 @@ public class base {
 		driver.quit();
 		
 
+	}
+	
+	@Test
+	public void testing() {
+		driver.get("https:\\www.facebook.com");
 	}
 
 
