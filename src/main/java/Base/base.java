c5b7b12612a22c282d67e6f7f2c3eab9e2769471
a-uzzaman <a.uzzaman1206@gmail.com>
2019-09-22 17:48:02 -0400

    test

--- src/main/java/Base/base.java
+++ src/main/java/Base/base.java
@@ -4,35 +4,49 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class base {
 
 	public WebDriver driver;
 
-	
+@Parameters("os")
 	@BeforeSuite
-	public void beforeSuite(){
-		
-		
-		System.out.println("*** TEST SUITE STARTED ");
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\Drivers\\chromedriver.exe");
-		
+	public void beforeSuite(String os){
+
+		switch(os) {
+		case "windows":
+
+			System.out.println("*** TEST SUITE STARTED ");
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\Drivers\\chromedriver.exe");
 
 
-		driver = new ChromeDriver();
-		driver.get("https://www.facebook.com");
-		driver.manage().window().maximize();
+
+			driver = new ChromeDriver();
+			driver.get("https://www.facebook.com");
+			driver.manage().window().maximize();
+		case "mac":
+
+			System.out.println("*** TEST SUITE STARTED ");
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\Drivers\\chromedriver");
+
+
+
+			driver = new ChromeDriver();
+			driver.get("https://www.facebook.com");
+			driver.manage().window().maximize();
 		}
-		
+	}
+
 	@AfterTest
 	public void afterSuite(){
 		System.out.println("*** TEST SUITE ENDED ");
 		driver.quit();
-		
+
 
 	}
-	
+
 	@Test
 	public void testing() {
 		driver.get("https:\\www.facebook.com");
@@ -42,5 +56,5 @@ public class base {
 
 
 }
-	
+
 
